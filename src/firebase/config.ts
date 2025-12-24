de55434e8bb4acdf5ff494c17e7a1ef1f7ad6851
Pedro Rafael <pedrorafael7337@gmail.com>
2025-12-24 00:07:43 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6541793467-33db6",
+  "appId": "1:55773513036:web:a8ee71e29548dc40e33be9",
+  "apiKey": "AIzaSyDBm3h0gPM1HSdN2Gv63c6l77CEw334L0c",
+  "authDomain": "studio-6541793467-33db6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "55773513036"
+};
