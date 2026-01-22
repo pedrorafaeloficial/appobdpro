b761a63b6fb3bad992718a8c5a6131ec9e087a0b
Pedro Rafael <94063903+pedrorafaeloficial@users.noreply.github.com>
2026-01-22 13:20:03 -0300

    Update logo.tsx

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,7 @@ import type { FC } from 'react';
 const Logo: FC = () => {
   return (
     <Image
-      src="https://sites-wp-obdpro.vlxcg6.easypanel.host/wp-content/uploads/2025/12/OBD-pro-Branco.png"
+      src="/src/OBD-pro-Branco.png"
       alt="OBD-Pro Logo"
       width={150}
       height={35}
